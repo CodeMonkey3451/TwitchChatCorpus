ef86998acc8b0a629a39edca917a2113399f44e4
CodeMonkey3451 <alexander.ekrheg@gmail.com>
2018-04-28 23:42:47 +0200

    Added more config options

--- train_twitch.py
+++ train_twitch.py
@@ -3,18 +3,22 @@ import json
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-TRAINPATH = "/home/alex/Workspace/ChatBotProject/train_data/"
+TRAINPATH = "./dialogs/"
 
 def PrintQuestResp(question):
 	"""Ask a question to get a respose. Print both."""
 	response = chatbot.get_response(question)
 	print("Bot: " + str(response))
 
+with open("config.json", 'r', encoding="utf-8") as file:
+    CONFIG = json.load(file)
+ChatterBotName = CONFIG["ChatterBotName"]
+ChatterBotDB = CONFIG["ChatterBotDatabase"]
 
+# Define ChatterBot instance
 chatbot = ChatBot(
-    'Monkalot',
-#    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
-    database="monkalot.db"
+    ChatterBotName,
+    database = ChatterBotDB
 )
 chatbot.set_trainer(ListTrainer)
 
@@ -30,8 +34,8 @@ for file in os.listdir(directory):
 				print(dia)
 				chatbot.train(dia)
 
+# Terminal interaction for testing
 print("Write something to start...")
-
 while True:
 	try:
 		userin = input("You: ")
